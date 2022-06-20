0f368fe2ce43991744bd86068e7f2d90a355903b
F4b1Maa5 <78047987+F4b1Maa5@users.noreply.github.com>
2022-06-20 20:50:05 +0200

    Add CSS Add Mac-Address

--- Tools/IPv42IPv6/js/Calculate.js
+++ Tools/IPv42IPv6/js/Calculate.js
@@ -1,5 +1,6 @@
 document.querySelector('#ipv4').onclick = function () {
 	var IPv4 = document.querySelector("#IPv4").value;
+	var IPv6Prefix = document.querySelector("#IPv6Prefix").value;
     var regex = /[0-9]{1,3}/g;
     var IPv4Parts = IPv4.match(regex);
     if(IPv4Parts.length != 4)
@@ -9,21 +10,18 @@ document.querySelector('#ipv4').onclick = function () {
     }
     IPv6Parts = [];
     IPv4Parts.forEach(quad => {
-        //console.log("quad " + quad);
         var firstpart = parseInt(parseInt(quad) / 16);
-        //console.log("firstpart "+firstpart);
         var secondpart = parseInt(quad) % 16;
-        //console.log("secondpart "+secondpart);
         var hex1 = gethexvalue(firstpart);
         var hex2 = gethexvalue(secondpart);
         IPv6Parts.push(hex1 + "" + hex2);
     });
     IPv6Parts = setsemicolon(IPv6Parts);
-    let string = "";
+    string = "";
     IPv6Parts.forEach(element => {
-        string =+ element;
+        string = string + element;
     });
-    document.querySelector('.output').innerHTML = string;
+    document.querySelector('.output').innerHTML = IPv6Prefix+ ":"+ string;
 }
 
 function gethexvalue(number) {
@@ -48,9 +46,54 @@ function gethexvalue(number) {
 
 function setsemicolon(array){
     let temparray = [];
-    for (let index = 0; index < array.length-1; index++) {
+    for (let index = 0; index < array.length-1; index+=2) {
         const element = array[index];
         temparray.push(":"+element +"" + array[index+1])
     }
     return temparray;
+}
+
+
+document.querySelector('#mac').onclick = function () {
+	var MAC = document.querySelector("#MAC").value;
+	var IPv6Prefix = document.querySelector("#IPv6Prefix").value;
+    MacArray = MAC.split(":")
+    
+    MacArray[0] = flipHex(MacArray[0]);
+
+    MacArray.splice(3,0,"FF");
+    MacArray.join();
+    MacArray.splice(4,0,"FE");
+    MacArray.join();
+
+
+    MacArray = setsemicolon(MacArray);
+    string = "";
+    MacArray.forEach(element => {
+        string = string + element;
+    });
+    document.querySelector('.output').innerHTML = IPv6Prefix + string;
+ }
+
+function flipHex(hextoflip){
+    HexArraySplit = hextoflip.split("",2);
+    console.log(HexArraySplit[1]);
+    bit = parseInt(HexArraySplit[1],16).toString(2);
+    console.log(bit);
+    bittoflip = bit.split("",4);
+    console.log(bittoflip[2]);
+    switch (bittoflip[2]) {
+        case "1":
+            bittoflip[2] = 0; 
+            break;
+        case "0":
+            bittoflip[2] = 1;
+            break
+        default:
+            throw new Error("Not Implemented");
+    }
+    flipped = bittoflip.join('');
+    hex = parseInt(flipped,2).toString(16).toUpperCase();
+    HexArraySplit[1] = hex;
+    return HexArraySplit.join('');
 }
